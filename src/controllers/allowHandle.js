bddc7885589a48e3307dbcddebcd706c52ab64be
Thanh Tran <tt222kj@student.lnu.se>
2022-02-21 16:48:50 +0100

    add content type

--- src/controllers/allowHandle.js
+++ src/controllers/allowHandle.js
@@ -10,12 +10,14 @@
       if (req.method === 'OPTIONS') {
         return res
           .status(204)
+          .setHeader('Content-Type', 'application/json')
           .header('Access-Control-Allow-Methods', req.allow)
           .send()
       }
       if (req.allow && !req.allow.includes(req.method)) {
         return res
           .status(405)
+          .setHeader('Content-Type', 'application/json')
           .header('Allow', req.allow)
           .send()
       }
